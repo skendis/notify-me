46b0889ea5c130e0b15b4af50937a80b9a7ac720
skendis <sergei.kendis@gmail.com>
2021-05-15 10:30:15 +0300

    fix

--- server.js
+++ server.js
@@ -7,50 +7,58 @@ const bodyparser = require('body-parser');
 const app = express();
 const appName = 'notify-me';
 const port = process.env.PORT || 8080;
-const notificationsUrl = 'https://www.oref.org.il/WarningMessages/alert/alerts.json';
-const historyUrl = 'https://www.oref.org.il//Shared/Ajax/GetAlarmsHistory.aspx?lang=he&mode=1';
-const configHeaders = {
-  'Accept': 'text/plain, */*; q=0.01',
-  'X-Requested-With': 'XMLHttpRequest',
-  'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-  'Referer': 'https://www.oref.org.il//12481-he/Pakar.aspx',
-  'Host':'www.oref.org.il',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
-}
+const notificationsUrl = process.env.ALERT_URL;
+const historyUrl = process.env.HISTORY_URL
+const configHeaders = process.env.REQ_HEADERS;
+const isExternalApi = process.env.IS_EXTERNAL_API;
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + `/dist/${appName}`));
 app.use(bodyparser.json());
 
-
 app.get('/api/notifications', async (req, res) => {
-  try {
-    const response = await axios.get(notificationsUrl, {headers: configHeaders});
-    console.log('response',response);
-    return res.status(200).json(response.data);
-  } catch (err) {
-    console.log('error:',err);
-    return res.status(503).json({
-      description:'server error',
-      data:err
-    })
+  if (isExternalApi) {
+    try {
+      const response = await axios.get(notificationsUrl, {headers: configHeaders});
+      return res.status(200).json(response.data);
+    } catch (err) {
+      return res.status(503).json({
+        description: 'server error',
+        data: err
+      })
+    }
+  } else {
+    try {
+      return res.status(200).sendFile(path.join(__dirname + '/data/alerts.json'));
+    } catch (err) {
+      return res.status(500).json({
+        description: 'server error',
+        data: err
+      })
+    }
   }
 })
 
-
-
-
 app.get('/api/notifications-history', async (req, res) => {
-  try {
-    const response = await axios.get(historyUrl,{headers:configHeaders});
-    console.log('response',response);
-    return res.status(200).json(response.data);
-  } catch (err) {
-    console.log('error:',err);
-    return res.status(503).json({
-      description:'server error',
-      data:err
-    })
+  if (isExternalApi) {
+    try {
+      const response = await axios.get(historyUrl, {headers: configHeaders});
+      return res.status(200).json(response.data);
+    } catch (err) {
+      return res.status(503).json({
+        description: 'server error',
+        data: err
+      })
+    }
+  } else {
+    try {
+      return res.status(200).sendFile(path.join(__dirname + '/data/history.json'));
+    } catch (err) {
+      return res.status(500).json({
+        description: 'server error',
+        data: err
+      })
+    }
   }
 })
 
@@ -62,3 +70,4 @@ app.get('/*', (req, res, next) => {
 
 // Start the app by listening on the default Heroku port
 app.listen(port, () => console.log('server listening on', port));
+
