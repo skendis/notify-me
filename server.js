a1acf32cf085b3ad3bbf18e169782f88f958269b
skendis <sergei.kendis@gmail.com>
2021-05-15 09:56:46 +0300

    fix

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const configHeaders = {
   'X-Requested-With': 'XMLHttpRequest',
   'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
   'Referer': 'https://www.oref.org.il//12481-he/Pakar.aspx',
-  'Host':'www.oref.org.il'
+  'Host':'www.oref.org.il',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
 }
-
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + `/dist/${appName}`));
@@ -26,6 +26,7 @@ app.use(bodyparser.json());
 app.get('/api/notifications', async (req, res) => {
   try {
     const response = await axios.get(notificationsUrl, {headers: configHeaders});
+    console.log('response',response);
     return res.status(200).json(response.data);
   } catch (err) {
     console.log('error:',err);
@@ -42,6 +43,7 @@ app.get('/api/notifications', async (req, res) => {
 app.get('/api/notifications-history', async (req, res) => {
   try {
     const response = await axios.get(historyUrl,{headers:configHeaders});
+    console.log('response',response);
     return res.status(200).json(response.data);
   } catch (err) {
     console.log('error:',err);
